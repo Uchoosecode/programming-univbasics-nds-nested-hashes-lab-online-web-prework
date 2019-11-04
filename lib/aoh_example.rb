d46713bc2f8dff5bb3f84b11dced7538dc4fc45b
Ayako Wiltshire <awittyl44@gmail.com>
2019-11-04 22:51:13 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -43,6 +43,6 @@ def aoh_update(aoh, row, key, new_value)
   local_copy_of_aoh[1][:name] = "Joelle van Dyne (PGOAT)"
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
-   result = local_copy_of_aoh 
-  return result[1][:name]
+   p result = local_copy_of_aoh 
+  p result[1][:name]
 end
