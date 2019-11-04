c417b51a8bb01503a0ee18c5e241bbd5b53e2f36
Ayako Wiltshire <awittyl44@gmail.com>
2019-11-04 22:18:08 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -22,12 +22,12 @@ end
 
 def literal_aoh
     result = [
-    DON_G = {:name=>"Don Gately", :occupation=>"Live-in Staff"},
-    JOELLE_VD = {:name=>"Joelle van Dyne", :occupation=>"Radio Personality"},
-    PAT_M = {:name=>"Pat Monteseian", :occupation=>"Staff"},
-    KATE_G = {:name=>"Kate Gompert", :occupation=>"None"},
-    BRUCE_G = {:name=>"Bruce Green", :occupation=>"Fan of Mildred"}
-    ]
+      DON_G,
+            JOELLE_VD,
+            PAT_M,
+            KATE_G,
+            RUCE_G
+            ]
   
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
