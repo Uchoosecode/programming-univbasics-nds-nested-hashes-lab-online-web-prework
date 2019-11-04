867a9e167891966d44e2b4dc72027c78cadd447f
Ayako Wiltshire <awittyl44@gmail.com>
2019-11-04 22:25:12 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -10,9 +10,7 @@ def assembled_aoh
   result = [
     {:name=>"Don Gately", :occupation=>"Live-in Staff"},
     {:name=>"Joelle van Dyne", :occupation=>"Radio Personality"},
-    {:name=>"Pat Monteseian", :occupation=>"Staff"},
-    {:name=>"Kate Gompert", :occupation=>"None"},
-    {:name=>"Bruce Green", :occupation=>"Fan of Mildred"}
+    {:name=>"Pat Monteseian", :occupation=>"Staff"}
     ]
  
   # Build an array that contains (or, "nests") the constants into a single
